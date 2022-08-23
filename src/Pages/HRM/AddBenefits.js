47ad71d82d185cc626bbb972fa6bb6a5b7ac1736
Ahmad Hassaan Ullah <ahmadullah943@gmail.com>
2022-08-23 14:45:44 +0500

    feat: Created page for adding benefits

---
+++ src/Pages/HRM/AddBenefits.js
@@ -0,0 +1,254 @@
+import PageTemplate from "../../Components/PageTemplate";
+import React, { useEffect, useState } from "react";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
+import Swal from "sweetalert2";
+
+function AddBenefits({ pagePermission }) {
+  const rolePermissionTable = {
+    Add: pagePermission.AddPermission,
+    Delete: pagePermission.DelPermission,
+    Edit: pagePermission.EditPermission,
+  };
+  const URL = localStorage.getItem("authUser");
+  const [isLoading, setIsLoading] = useState(true);
+  const [updateMode, setUpdateMode] = useState(false);
+  const [listingData, setListingData] = useState([]);
+  const [initialValues, setInitialValues] = useState({
+    benefit_id: 0,
+    benefit_title: "",
+  });
+
+  let formFields = [
+    {
+      label: "Benefit Title",
+      name: "benefit_title",
+      type: "text",
+      placeholder: "Enter Benefit Title",
+      required: true,
+      disabled: false,
+      hidden: false,
+    },
+  ];
+
+  const DisplayingErrorMessagesSchema = Yup.object().shape({
+    benefit_title: Yup.string().required("Select Benefit First"),
+  });
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const clearFields = () => {
+    setInitialValues({
+      benefit_id: 0,
+      benefit_title: "",
+    });
+    setUpdateMode(false);
+  };
+
+  const changeFieldValue = (field, value) => {
+    var obj = initialValues;
+    obj.benefit_title = value;
+    setInitialValues(obj);
+  };
+
+  const fetchData = () => {
+    fetch(URL + "api/Benefits", {
+      method: "GET",
+      headers: {
+        Authorization:
+          "Bearer " +
+          JSON.parse(localStorage.getItem("access_token")).access_token,
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        var theadData = [
+          {
+            heading: "SR #",
+            className: "",
+            style: {},
+          },
+          {
+            heading: "Benefit Title",
+            className: "",
+            style: {},
+          },
+          {
+            heading: "Actions",
+            className: "text-right",
+            style: {},
+          },
+        ];
+
+        var tbodyData = [];
+        json.map((value, index) => {
+          tbodyData.push([
+            {
+              name: "sr_no",
+              value: index + 1,
+              className: "",
+              style: {},
+            },
+            {
+              name: "benefit_title",
+              value: value.benefit_title,
+              className: "",
+              style: {},
+            },
+            {
+              name: "actions",
+              value: "",
+              icons: [
+                {
+                  type: "edit",
+                  clickFunction: fetchDataForEdit,
+                },
+                {
+                  type: "delete",
+                  clickFunction: deleteBenefit,
+                },
+              ],
+              className: "text-right",
+              style: {
+                width: "20%",
+              },
+              entity_id: value.benefit_id,
+            },
+          ]);
+        });
+
+        setListingData([
+          {
+            title: "Listing",
+            icon: "fa fa-list",
+            theadData,
+            tbodyData,
+          },
+        ]);
+
+        setIsLoading(false);
+      });
+  };
+
+  const fetchDataForEdit = (benefit_id) => {
+    setUpdateMode(false);
+
+    fetch(URL + "api/Benefits/" + benefit_id, {
+      method: "GET",
+      headers: {
+        Authorization:
+          "Bearer " +
+          JSON.parse(localStorage.getItem("access_token")).access_token,
+      },
+
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.benefit_title === null) {
+          json.benefit_title = "";
+        }
+
+        let obj = initialValues;
+        obj.benefit_id = json.benefit_id;
+        obj.benefit_title = json.benefit_title;
+        //console.log(obj);
+        setInitialValues(obj);
+        setUpdateMode(true);
+      });
+  };
+
+  const formSubmit = (data) => {
+    //console.log(data, "adadada");
+
+    fetch(
+      URL +
+      (updateMode
+        ? "/api/Benefits/" +
+        initialValues.benefit_id
+        : "/api/Benefits/"),
+      {
+        method: updateMode ? "PUT" : "POST",
+        headers: {
+          Authorization:
+            "Bearer " +
+            JSON.parse(localStorage.getItem("access_token")).access_token,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          "benefit_id": initialValues.benefit_id,
+          "benefit_title": initialValues.benefit_title
+        }),
+      }
+    ).then((response) => {
+      if (response.status === 201 || response.status === 400) {
+
+        toast.error("Something went wrong");
+      } else {
+        toast.success(
+          "Benefit Title has been " +
+          (updateMode ? "Updated" : "Added" + " successfully!")
+        );
+        clearFields();
+        fetchData();
+      }
+    });
+  };
+
+  const deleteBenefit = (benefit_id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this 'Benefit'!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Do it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(URL + "api/Benefits/" + benefit_id, {
+          method: "DELETE",
+          headers: {
+            Authorization:
+              "Bearer " +
+              JSON.parse(localStorage.getItem("access_token")).access_token,
+          },
+        }).then((response) => {
+          if (response.status === 200) {
+            toast.success("Benefit has been Deleted successfully!");
+            fetchData();
+            clearFields();
+          } else {
+            response.json().then((json) => {
+              toast.error(json.Message);
+            });
+          }
+        });
+      }
+    });
+  };
+
+  return (
+    <PageTemplate
+      pagePermission={rolePermissionTable}
+      moduleName="Manage Benefits"
+      formTitle="Add/Edit Benefit Title"
+      formFields={formFields}
+      initialValues={initialValues}
+      DisplayingErrorMessagesSchema={DisplayingErrorMessagesSchema}
+      updateMode={updateMode}
+      formSubmit={formSubmit}
+      listing={listingData}
+      isLoading={isLoading}
+      setIsLoading={setIsLoading}
+      fetchData={fetchData}
+      clearFields={clearFields}
+      isShowSelector={true}
+      changeFieldValue={changeFieldValue}
+      showButtons={true}
+    />
+  );
+}
+
+export default AddBenefits;
--- src/config/ROUTES.js
+++ src/config/ROUTES.js
@@ -1,4 +1,3 @@
-
 import Dashboard from ".././Pages/Home/Dashboard.js/Dashboard";
 import AddRole from ".././Pages/Role/AddRole";
 import AddUser from ".././Pages/Role/AddUser";
@@ -34,46 +33,44 @@ import PLStatement from "../Pages/Finance/Reports/PLStatement/PLStatement";
 import TrialSheet from "../Pages/Finance/Reports/TrialBalance/TrialSheet";
 import TransactionReport from "../Pages/Finance/Reports/TransactionReport/TransactionReport";
 import OpeningBalances from "../Pages/Finance/OpeningBalances";
-
-
-
+import AddBenefits from "../Pages/HRM/AddBenefits";
 
 const ROUTES = {
-    AddRole: { id: '1', element: AddRole, path: "RoleAccess" },
-    VoucherHistory: { id: '2', element: VoucherHistory, path: "VoucherHistoryAccess" },
-    AddModules: { id: '3', element: AddModules, path: "ModuleAccess" },
-    AddUser: { id: '4', element: AddUser, path: "UserAccess" },
-    TestDataTable: { id: '5', element: TestDataTable, path: "TestDataTable" },
-    BranchesPermission: { id: '6', element: BranchesPermission, path: "BranchesPermissionAccess" },
-    BankPaymentVoucher: { id: '7', element: BankPaymentVoucher, path: "BankPaymentAccess" },
-    BankReceiveVoucher: { id: '8', element: BankReceiveVoucher, path: "BankReceiveAccess" },
-    CashPaymentVoucher: { id: '10', element: CashPaymentVoucher, path: "CashPaymentAccess" },
-    CashReceiveVoucher: { id: '11', element: CashReceiveVoucher, path: "CashReceiveAccess" },
-    JournalVoucher: { id: '12', element: JournalVoucher, path: "JournalVoucherAccess" },
-    JournalVoucherSingle: { id: '13', element: JournalVoucherSingle, path: "JournalVoucherSingleAccess" },
-    PayableReceivableReport: { id: '14', element: PayableReceivableReport, path: "PayableReceivableReportAccess" },
-    GeneralLadger: { id: '15', element: GeneralLadger, path: "LadgerAccess" },
-    RolePermission: { id: '16', element: RolePermission, path: "PermissionAccess" },
-    AddPages: { id: '17', element: AddPages, path: "PagesAccess" },
-    Dashboard: { id: '18', element: Dashboard, path: "/" },
-    CompanyInfo: { id: '19', element: CompanyInfo, path: "CompanyInfo" },
-    Banknames: { id: '20', element: Banknames, path: "Banknames" },
-    EmployeeDesignations: { id: '21', element: EmployeeDesignations, path: "Designation" },
-    CurrencyUnits: { id: '22', element: CurrencyUnits, path: "CurrencyUnits" },
-    AddMachines: { id: '23', element: AddMachines, path: "Machines" },
-    AddBranches: { id: '24', element: AddBranches, path: "AddBranchesAccess" },
-    AddShifts: { id: '25', element: AddShifts, path: "Shifts" },
-    CityNames: { id: '26', element: CityNames, path: "CityNames" },
-    StockUnits: { id: '27', element: StockUnits, path: "StockUnits" },
-    Categories: { id: '28', element: Categories, path: "Categories" },
-    BankBookLadger: { id: '29', element: BankBookLadger, path: "BankBookAccess" },
-    CashBookLadger: { id: '30', element: CashBookLadger, path: "CashBookAccess" },
-    TrialSheet: { id: '31', element: TrialSheet, path: "TrialBalanceAccess" },
-    PLStatement: { id: '32', element: PLStatement, path: "PLStatementAccess" },
-    BalanceSheet: { id: '33', element: BalanceSheet, path: "BalanceSheetAccess" },
-    TransactionReport: { id: '34', element: TransactionReport, path: "TransactionReportAccess" },
-    OpeningBalances: { id: '35', element: OpeningBalances, path: "OpeningBalancesAccess" },
+  AddRole: { id: '1', element: AddRole, path: "RoleAccess" },
+  VoucherHistory: { id: '2', element: VoucherHistory, path: "VoucherHistoryAccess" },
+  AddModules: { id: '3', element: AddModules, path: "ModuleAccess" },
+  AddUser: { id: '4', element: AddUser, path: "UserAccess" },
+  TestDataTable: { id: '5', element: TestDataTable, path: "TestDataTable" },
+  BranchesPermission: { id: '6', element: BranchesPermission, path: "BranchesPermissionAccess" },
+  BankPaymentVoucher: { id: '7', element: BankPaymentVoucher, path: "BankPaymentAccess" },
+  BankReceiveVoucher: { id: '8', element: BankReceiveVoucher, path: "BankReceiveAccess" },
+  CashPaymentVoucher: { id: '10', element: CashPaymentVoucher, path: "CashPaymentAccess" },
+  CashReceiveVoucher: { id: '11', element: CashReceiveVoucher, path: "CashReceiveAccess" },
+  JournalVoucher: { id: '12', element: JournalVoucher, path: "JournalVoucherAccess" },
+  JournalVoucherSingle: { id: '13', element: JournalVoucherSingle, path: "JournalVoucherSingleAccess" },
+  PayableReceivableReport: { id: '14', element: PayableReceivableReport, path: "PayableReceivableReportAccess" },
+  GeneralLadger: { id: '15', element: GeneralLadger, path: "LadgerAccess" },
+  RolePermission: { id: '16', element: RolePermission, path: "PermissionAccess" },
+  AddPages: { id: '17', element: AddPages, path: "PagesAccess" },
+  Dashboard: { id: '18', element: Dashboard, path: "/" },
+  CompanyInfo: { id: '19', element: CompanyInfo, path: "CompanyInfo" },
+  Banknames: { id: '20', element: Banknames, path: "Banknames" },
+  EmployeeDesignations: { id: '21', element: EmployeeDesignations, path: "Designation" },
+  CurrencyUnits: { id: '22', element: CurrencyUnits, path: "CurrencyUnits" },
+  AddMachines: { id: '23', element: AddMachines, path: "Machines" },
+  AddBranches: { id: '24', element: AddBranches, path: "AddBranchesAccess" },
+  AddShifts: { id: '25', element: AddShifts, path: "Shifts" },
+  CityNames: { id: '26', element: CityNames, path: "CityNames" },
+  StockUnits: { id: '27', element: StockUnits, path: "StockUnits" },
+  Categories: { id: '28', element: Categories, path: "Categories" },
+  BankBookLadger: { id: '29', element: BankBookLadger, path: "BankBookAccess" },
+  CashBookLadger: { id: '30', element: CashBookLadger, path: "CashBookAccess" },
+  TrialSheet: { id: '31', element: TrialSheet, path: "TrialBalanceAccess" },
+  PLStatement: { id: '32', element: PLStatement, path: "PLStatementAccess" },
+  BalanceSheet: { id: '33', element: BalanceSheet, path: "BalanceSheetAccess" },
+  TransactionReport: { id: '34', element: TransactionReport, path: "TransactionReportAccess" },
+  OpeningBalances: { id: '35', element: OpeningBalances, path: "OpeningBalancesAccess" },
+  AddBenefits: { id: "36", element: AddBenefits, path: "BenefitsAccess" },
 };
 
 export default ROUTES;
-
