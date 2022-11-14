8351cabfb02ea7b3f471e3461212f3a08d2f09fe
Junaid Butt <areej.amjad14@gmail.com>
2022-11-14 17:40:24 +0500

    .

--- src/Pages/Production/ProductionReport.js
+++ src/Pages/Production/ProductionReport.js
@@ -1,4 +1,4 @@
-import React, { useState,useRef } from "react";
+import React, { useState,useRef,useEffect } from "react";
 import { useSelector } from "react-redux";
 import Select from "react-select";
 import axios from "axios";
